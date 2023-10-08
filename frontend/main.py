63e2960f318da1d3affd58f08d9843ffccf48f59
rbrox <rishavsci@gmail.com>
2023-10-08 07:38:58 +0530

    linking

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,6 @@
+from process import *
 import streamlit as st
+import os
 
 def main():
     st.title("PDF Uploader")
@@ -6,6 +8,14 @@ def main():
     if uploaded_file is not None:
         st.write("You selected the following file:")
         st.write(uploaded_file)
+        if not os.path.exists('./data/New_Sent'):
+            os.makedirs('./data/New_Sent')
+        path = './data/New_Sent/' + uploaded_file.name
+        with open(path, 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+
+    results = analyze_resume(path)
+    st.write(results)
 
 if __name__ == "__main__":
     main()
